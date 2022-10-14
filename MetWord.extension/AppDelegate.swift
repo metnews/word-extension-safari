bc259718971a2a1bf5794bf9be52795c5a994237
Jian Li <kdlijian@gmail.com>
2022-10-14 19:35:43 +0800

    initial commit

---
+++ MetWord.extension/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  MetWord.extension
+//
+//  Created by Li Jian on 2022/6/10.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Override point for customization after application launch.
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
